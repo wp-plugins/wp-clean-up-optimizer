039c3d2e33d30cdb8ef7dcd0861cd50381161ef4
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-11 11:36:49 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/wp-clean-up-optimizer/trunk@1132694 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/cleanup-optimizer-class.php
+++ lib/cleanup-optimizer-class.php
@@ -55,10 +55,6 @@ else
 				(
 					"DELETE FROM $wpdb->commentmeta WHERE comment_id NOT IN (SELECT comment_id FROM $wpdb->comments)"
 				);
-				$wpdb->query
-				(
-					"DELETE FROM $wpdb->comments WHERE comment_approved = 0"
-				);
 			break;
 			
 			case 6:
